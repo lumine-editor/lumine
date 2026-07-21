3f493803d9f3e09a4a13d712575ca5c760750b4e
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-07-21 18:03:33 +0200

    Replace @pulsar-edit/ls-archive with modernized @lumine-code/ls-archive fork

--- packages/archive-view/lib/archive.js
+++ packages/archive-view/lib/archive.js
@@ -1,15 +1,5 @@
 /** @babel */
 
-import util from "util";
-
-const isErrorProperty = "isError";
-
-if (typeof util[isErrorProperty] !== "function") {
-  util[isErrorProperty] = (value) => {
-    return value instanceof Error || Object.prototype.toString.call(value) === "[object Error]";
-  };
-}
-
-const archive = require("@pulsar-edit/ls-archive");
+const archive = require("@lumine-code/ls-archive");
 
 export default archive;
